9e2b5217d42557c9433b91407ba33db58f4c89d2
RobbeVanslambrouck1 <robbe.vanslambrouck1@student.kuleuven.be>
2022-03-01 18:25:49 +0100

    added class heading to File

--- src/File.java
+++ src/File.java
@@ -1,2 +1,20 @@
+/**
+ * the file class has the following objects: name, size, creation_time, modification_time, writable
+ *
+ * @version 1.0
+ * @author Robbe Vanslambrouck
+ * @author
+ * @author
+ */
+
 public class File {
+
+    // class objects
+    public int size;
+
+    // constructors
+
+    // getters & setters
+
+    // class methods
 }
