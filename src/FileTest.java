409b2de9bcce05b99199ee693cc31b4f454e5450
Quinten Steeland <quinten.steeland@student.kuleuven.be>
2022-03-03 11:17:16 +0100

    Add test

---
+++ src/FileTest.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class FileTest {
+
+    private File file1;
+
+    @Before
+    public void runBeforeEveryTest() {
+        file1 = new File("filename");
+    }
+
+    @Test
+    public void testEmptyName() {
+        file1.setName("");
+        assertEquals(file1.getName(), ".");
+    }
+}
